a1052a9737c814830bc479e976d5bd1dfcb5e241
ALIEN <matthias.moulin@gmail.com>
2017-04-19 11:06:47 +0200

    Update

--- MAGE-Doc/html/classmage_1_1_orthographic_camera.js
+++ MAGE-Doc/html/classmage_1_1_orthographic_camera.js
@@ -1,12 +1,19 @@
 var classmage_1_1_orthographic_camera =
 [
-    [ "OrthographicCamera", "classmage_1_1_orthographic_camera.html#a7d8b48144b0ef2e2f0aa4f06f3858abd", null ],
+    [ "OrthographicCamera", "classmage_1_1_orthographic_camera.html#a071c7183ece84b94b1f658c7220deb30", null ],
     [ "OrthographicCamera", "classmage_1_1_orthographic_camera.html#a0293f01356417e9f32db027ba92d6029", null ],
     [ "OrthographicCamera", "classmage_1_1_orthographic_camera.html#a6abe78fc17b583046b30a459f9ef07ef", null ],
     [ "~OrthographicCamera", "classmage_1_1_orthographic_camera.html#abdad923634e17f217ba975a9149f6c57", null ],
     [ "Clone", "classmage_1_1_orthographic_camera.html#ae075c08e4af88f74212bf5c84d2e5b2a", null ],
+    [ "GetHeight", "classmage_1_1_orthographic_camera.html#a596e2cf22fee8cf30139ffe9d4cdcda7", null ],
     [ "GetViewToProjectionMatrix", "classmage_1_1_orthographic_camera.html#aedd86e56a0f7bc967ad8d9be2631a0cf", null ],
-    [ "operator=", "classmage_1_1_orthographic_camera.html#a2708bae64577677d4d5d1722a9ce0e9f", null ],
-    [ "operator=", "classmage_1_1_orthographic_camera.html#aaf29c1fcc52d0a5e6cfdb65b9c840174", null ],
-    [ "SetViewToProjectionMatrix", "classmage_1_1_orthographic_camera.html#a1ff2b3e4467049b978155d652a687c2d", null ]
+    [ "GetWidth", "classmage_1_1_orthographic_camera.html#ac8daeca630e0f4503354e3eede7ead5c", null ],
+    [ "operator=", "classmage_1_1_orthographic_camera.html#a38631b1c377f0e3b6fa99ac5535ce226", null ],
+    [ "operator=", "classmage_1_1_orthographic_camera.html#a51b38eab8945fb1a2031a7ba1755d689", null ],
+    [ "SetHeight", "classmage_1_1_orthographic_camera.html#ac9c1f87e2cf085b7c62d7f6e34eeb944", null ],
+    [ "SetViewToProjectionMatrix", "classmage_1_1_orthographic_camera.html#a10e09af47e741ab76dc2aa2a03f04c06", null ],
+    [ "SetWidth", "classmage_1_1_orthographic_camera.html#a7687aef78ac2786be1c9ff69f396859a", null ],
+    [ "SetWidthAndHeight", "classmage_1_1_orthographic_camera.html#a11319878a43113e54be50812a786343c", null ],
+    [ "m_height", "classmage_1_1_orthographic_camera.html#a63169098f604874c1b30c4b276b5a3e1", null ],
+    [ "m_width", "classmage_1_1_orthographic_camera.html#aadef4cff19cc1b1ecf427f82bbc3ea6a", null ]
 ];
--- MAGE-Doc/html/search/classes_3.js
+++ MAGE-Doc/html/search/classes_3.js
@@ -5,5 +5,6 @@ var searchData=
   ['dds_5fpixelformat',['DDS_PIXELFORMAT',['../structmage_1_1_d_d_s___p_i_x_e_l_f_o_r_m_a_t.html',1,'mage']]],
   ['deviceenumeration',['DeviceEnumeration',['../classmage_1_1_device_enumeration.html',1,'mage']]],
   ['direction3',['Direction3',['../structmage_1_1_direction3.html',1,'mage']]],
+  ['directionallight',['DirectionalLight',['../classmage_1_1_directional_light.html',1,'mage']]],
   ['dropshadowspritetext',['DropshadowSpriteText',['../classmage_1_1_dropshadow_sprite_text.html',1,'mage']]]
 ];
