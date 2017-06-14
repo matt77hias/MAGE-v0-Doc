b4b68339480c6f159c380c8bec247bb8789548df
ALIEN <matthias.moulin@gmail.com>
2017-06-14 12:14:11 +0200

    Update

--- MAGE-Doc/html/search/classes_11.js
+++ MAGE-Doc/html/search/classes_11.js
@@ -2,5 +2,7 @@ var searchData=
 [
   ['textconsolescript',['TextConsoleScript',['../classmage_1_1_text_console_script.html',1,'mage']]],
   ['texture',['Texture',['../classmage_1_1_texture.html',1,'mage']]],
-  ['timer',['Timer',['../classmage_1_1_timer.html',1,'mage']]]
+  ['timer',['Timer',['../classmage_1_1_timer.html',1,'mage']]],
+  ['tsnmpixelshader',['TSNMPixelShader',['../classmage_1_1_t_s_n_m_pixel_shader.html',1,'mage']]],
+  ['tsnmvertexshader',['TSNMVertexShader',['../classmage_1_1_t_s_n_m_vertex_shader.html',1,'mage']]]
 ];
